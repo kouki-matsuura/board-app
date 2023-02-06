5d107fa4fbe2b86dbe976ad3bcec5e6abf5f41c3
松浦　昂生 <kouki19991222@icloud.com>
2023-02-06 22:07:51 +0900

    登録機能の追加

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,16 +1,16 @@
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 import bcrypt from 'bcrypt'
-import { db } from "../../lib/prisma";
+import { db } from "$lib/prisma";
 
 export const actions: Actions = {
-    register : async ({request}) => {
+    register : async ({request }) => {
         const data = await request.formData();
         const name = data.get("name");
         const password = data.get("password");
-        
+
         if (typeof name !== "string" ||typeof password !== "string" || !name || !password) {
-            return fail(400, { invalid: true })
+            return fail(400, { message: "名前・パスワードは必須です。" })
         }
 
         const user = await db.user.findUnique({
@@ -18,7 +18,7 @@ export const actions: Actions = {
         })
 
         if (user) {
-            return fail(400, { invalid: true })
+            return fail(400, { message: "既に存在するユーザーです。" })
         }
         
         await db.user.create({
@@ -28,6 +28,6 @@ export const actions: Actions = {
                 authToken: crypto.randomUUID(),
             },
         })
-    throw redirect(303, '/login')
+        throw redirect(303, '/login')
     }
 }
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,6 +1,12 @@
-
+<script lang="ts">
+    import type { ActionData } from "./$types"
+    export let form: ActionData
+</script>
 <h1>ユーザー登録</h1>
-<form method="post" action="/register">
+{#if form?.message}
+  <p class="error">{form?.message}</p>
+{/if}
+<form method="post" action="?/register">
     <label>名前：
         <input name="name" type="text"/>
     </label>
@@ -9,3 +15,8 @@
     </label>
     <button>登録する</button>
 </form>
+<style>
+    .error {
+        color: red;
+    }
+</style>
