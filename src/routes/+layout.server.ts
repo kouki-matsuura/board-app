b5b1f55db0566b624e6e6b09755e30dd73808623
松浦　昂生 <kouki19991222@icloud.com>
2023-02-06 22:07:33 +0900

    layout.serverとhooks.serverの追加

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,13 @@
+import { db } from "$lib/prisma";
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ url, cookies, locals }) => {
+    const session = await cookies.get('session');
+    if(!session && (url.pathname !== '/login' && url.pathname!== '/register')) {
+        throw redirect(303, '/login')
+    }
+    return {
+        user:locals.user
+    }
+}
